c9b6dc06aded8d5901038c32f8e2a9d97085000f
elianezanlorense <eliane.zanlorense@gmail.com>
2026-03-24 18:12:08 +0100

    add bootstrap state files

---
+++ bootstrap/state/outputs.tf
@@ -0,0 +1,4 @@
+output "bucket_name" {
+  description = "Nome do bucket criado para guardar o terraform state"
+  value       = google_storage_bucket.terraform_state.name
+}
---
+++ bootstrap/state/state.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "terraform_state" {
+  name                        = var.bucket_name
+  location                    = var.location
+  uniform_bucket_level_access = true
+  force_destroy               = false
+
+  versioning {
+    enabled = true
+  }
+}
