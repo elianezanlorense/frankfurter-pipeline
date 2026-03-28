691b5a7c3d223136c455691672a81d9b1dee51f3
elianezanlorense <eliane.zanlorense@gmail.com>
2026-03-28 21:25:30 +0100

    update bucket name

--- bootstrap/state/outputs.tf
+++ bootstrap/state/outputs.tf
@@ -1,4 +1,4 @@
 output "bucket_name" {
   description = "Nome do bucket criado para guardar o terraform state"
-  value       = google_storage_bucket.terraform_state.bucket
+  value       = google_storage_bucket.terraform_state.name
 }
