006109c8fd5f76fcfce68b6802507f4fd3fd3c10
elianezanlorense <eliane.zanlorense@gmail.com>
2026-03-28 21:33:09 +0100

    fix: add GCS backend and import existing bucket

--- bootstrap/state/state.tf
+++ bootstrap/state/state.tf
@@ -1,11 +1,15 @@
 terraform {
   required_version = ">= 1.0"
-
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
+  }
+
+  backend "gcs" {
+    bucket = "frankfurter-tf-state"
+    prefix = "bootstrap/state"
   }
 }
 
