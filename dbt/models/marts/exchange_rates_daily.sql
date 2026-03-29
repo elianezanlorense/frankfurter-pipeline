bf962af181b780c7c9340e26bdf04346c9d804d2
elianezanlorense <eliane.zanlorense@gmail.com>
2026-03-29 18:41:55 +0200

    -dbt

---
+++ dbt/models/marts/exchange_rates_daily.sql
@@ -0,0 +1,20 @@
+with base as (
+    select
+        date,
+        base_currency,
+        target_currency,
+        rate
+    from {{ ref('stg_exchange_rates') }}
+)
+select
+    date,
+    base_currency,
+    target_currency,
+    rate,
+    AVG(rate) OVER (
+        PARTITION BY target_currency
+        ORDER BY date
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) as rate_7day_avg
+from base
+order by date desc
