de1217ad891f987d1c82bff37a152d4ba8473358
elianezanlorense <eliane.zanlorense@gmail.com>
2026-03-30 13:07:38 +0200

    fix: restore state.tf with resource

---
+++ bootstrap/state/state.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "terraform_state" {
+  name                        = var.bucket_name
+  location                    = var.location
+  uniform_bucket_level_access = true
+  force_destroy               = true
+
+  versioning {
+    enabled = true
+  }
+}
