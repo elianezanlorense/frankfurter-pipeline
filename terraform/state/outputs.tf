fb12ff8ec26236c69e01a4712087790d1016d2fa
elianezanlorense <eliane.zanlorense@gmail.com>
2026-05-07 09:11:03 +0200

    add

--- terraform/state/outputs.tf
+++ terraform/state/outputs.tf
@@ -11,5 +11,6 @@ output "terraform_runner_sa_email" {
 }
 
 output "workload_identity_provider" {
-  value = "projects/${google_project.my_project.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github.workload_identity_pool_id}/providers/${google_iam_workload_identity_pool_provider.github.workload_identity_pool_provider_id}"
+  # Isso retorna o caminho completo e formatado automaticamente pelo GCP
+  value = google_iam_workload_identity_pool_provider.github.name
 }
