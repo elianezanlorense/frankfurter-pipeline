132efba568d134553ab0d6d17bd100b1124c06d4
elianezanlorense <58109901+elianezanlorense@users.noreply.github.com>
2026-08-10 16:37:18 +0000

    update permisiions

--- terraform/state/state.tf
+++ terraform/state/state.tf
@@ -97,3 +97,9 @@ resource "google_project_iam_member" "terraform_workload_identity_admin" {
   role    = "roles/iam.workloadIdentityPoolAdmin"
   member  = "serviceAccount:${google_service_account.terraform_runner.email}"
 }
+
+resource "google_project_iam_member" "terraform_service_account_admin" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountAdmin"
+  member  = "serviceAccount:${google_service_account.terraform_runner.email}"
+}
