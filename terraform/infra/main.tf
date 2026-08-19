c5144a874ddbfa9bb9974c31c9c89a4b2790d5e8
elianezanlorense <eliane.zanlorense@gmail.com>
2026-08-19 18:12:34 +0200

    add gke

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -20,35 +20,6 @@ data "google_project" "project" {
   project_id = var.project_id
 }
 
-# --- PERMISSÕES IAM (ATUALIZADO) ---
-
-# Permissão para GitHub Actions
-resource "google_service_account_iam_member" "allow_github_to_use_compute_sa" {
-  service_account_id = "projects/${var.project_id}/serviceAccounts/${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  role               = "roles/iam.serviceAccountUser"
-  member             = "serviceAccount:github-actions-tf@${var.project_id}.iam.gserviceaccount.com"
-}
-
-# Permissão para VM escrever no Storage (Resolve erro save_to_gcs)
-resource "google_project_iam_member" "vm_storage_admin" {
-  project = var.project_id
-  role    = "roles/storage.objectAdmin"
-  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-}
-
-# Permissão para VM carregar dados no BigQuery (Resolve erro load_to_bigquery)
-resource "google_project_iam_member" "vm_bigquery_editor" {
-  project = var.project_id
-  role    = "roles/bigquery.dataEditor"
-  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-}
-
-resource "google_project_iam_member" "vm_bigquery_user" {
-  project = var.project_id
-  role    = "roles/bigquery.user"
-  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-}
-
 # --- DATA LAKE (STORAGE) ---
 resource "google_storage_bucket" "data_lake" {
   name                        = "${var.project_id}-data-lake"
@@ -84,68 +55,60 @@ resource "google_bigquery_table" "exchange_rates" {
   deletion_protection = false
 }
 
-# --- COMPUTE ENGINE (AIRFLOW VM) ---
-resource "google_compute_instance" "airflow_vm" {
-  name         = var.vm_name
-  machine_type = var.vm_machine_type
-  zone         = var.zone
-  project      = var.project_id
-
-  boot_disk {
-    initialize_params {
-      image = var.vm_image
-      size  = var.vm_disk_size
-      type  = "pd-standard"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {}
-  }
-
-  tags = ["airflow"]
-
-  metadata = {
-    ssh-keys = "${var.ssh_user}:${var.ssh_public_key}"
-  }
-
-  metadata_startup_script = file("${path.module}/startup_script.sh")
-
-  service_account {
-    email  = "${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-    scopes = ["cloud-platform"]
-  }
-
-  lifecycle {
-    replace_triggered_by = [
-      terraform_data.ssh_key,
-      terraform_data.startup_script
-    ]
-  }
-
-  depends_on = [google_service_account_iam_member.allow_github_to_use_compute_sa]
-}
-
-resource "terraform_data" "ssh_key" {
-  input = var.ssh_public_key
-}
-
-resource "terraform_data" "startup_script" {
-  input = filesha256("${path.module}/startup_script.sh")
-}
-
-# --- FIREWALL ---
-resource "google_compute_firewall" "allow_airflow" {
-  name    = "allow-airflow-ui"
-  network = "default"
+# ---------------------------------------------------------------------------
+# GKE Autopilot cluster para rodar Airflow (Helm) + dbt (KubernetesPodOperator)
+# ---------------------------------------------------------------------------
+resource "google_project_service" "container" {
   project = var.project_id
+  service = "container.googleapis.com"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["8080", "22"]
+  disable_on_destroy = false
+}
+
+resource "google_container_cluster" "airflow_gke" {
+  name     = "${var.project_id}-airflow-gke"
+  location = var.region
+
+  enable_autopilot = true
+
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["airflow"]
+  network    = "default"
+  subnetwork = "default"
+
+  deletion_protection = false
+
+  depends_on = [google_project_service.container]
+}
+
+resource "google_service_account" "airflow_gke_sa" {
+  project      = var.project_id
+  account_id   = "airflow-gke-sa"
+  display_name = "Airflow (GKE) Service Account"
+}
+
+resource "google_project_iam_member" "airflow_gke_sa_bigquery_editor" {
+  project = var.project_id
+  role    = "roles/bigquery.dataEditor"
+  member  = "serviceAccount:${google_service_account.airflow_gke_sa.email}"
+}
+
+resource "google_project_iam_member" "airflow_gke_sa_bigquery_job" {
+  project = var.project_id
+  role    = "roles/bigquery.jobUser"
+  member  = "serviceAccount:${google_service_account.airflow_gke_sa.email}"
+}
+
+resource "google_project_iam_member" "airflow_gke_sa_storage" {
+  project = var.project_id
+  role    = "roles/storage.objectAdmin"
+  member  = "serviceAccount:${google_service_account.airflow_gke_sa.email}"
+}
+
+resource "google_service_account_iam_member" "airflow_gke_sa_workload_identity" {
+  service_account_id = google_service_account.airflow_gke_sa.name
+  role                = "roles/iam.workloadIdentityUser"
+  member              = "serviceAccount:${var.project_id}.svc.id.goog[airflow/airflow]"
 }
--- terraform/infra/variables.tf
+++ terraform/infra/variables.tf
@@ -22,37 +22,3 @@ variable "bigquery_dataset" {
   type    = string
   default = "frankfurter_dev"
 }
-
-variable "zone" {
-  type    = string
-  default = "europe-west1-b"
-}
-
-variable "vm_name" {
-  type    = string
-  default = "airflow-vm"
-}
-
-variable "vm_machine_type" {
-  type    = string
-  default = "e2-medium"
-}
-
-variable "vm_image" {
-  type    = string
-  default = "debian-cloud/debian-12"
-}
-
-variable "vm_disk_size" {
-  type    = number
-  default = 20
-}
-
-variable "ssh_user" {
-  type    = string
-  default = "airflow"
-}
-
-variable "ssh_public_key" {
-  type = string
-}
